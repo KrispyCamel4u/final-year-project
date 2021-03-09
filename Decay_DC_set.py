28f9e8e46284ec1d08c887de76da9cafd3b912ec
mouna krishna <mounakrishna27121999@gmail.com>
2021-03-09 17:32:15 +0530

    SCripts

---
+++ Decay_DC_set.py
@@ -0,0 +1,76 @@
+import math
+import csv
+
+
+### parameters ###
+N = 100
+Rm = 150
+f = 50
+theta = 10
+
+w = 2*math.pi*f
+T = 1/(N*f)
+##################
+
+def compute(t, gamma, tou):
+    actual = Rm * math.sin(w*T*t + math.radians(theta))
+
+    with_off = actual + gamma * math.exp(-1*(T*t)/tou)
+
+    return [round(actual, 4), round(with_off, 4)]
+
+
+if __name__ == "__main__":
+
+    ### Fixed tou with varying mag ###
+    output_mag_err = []
+    output_mag_act = []
+    tou = 0.03
+    for i in range(1, 15*N+1):
+        temp1 = [round(i*T, 4)]
+        temp2 = [round(i*T, 4)]
+        for j in range(3, 10, 2):
+            temp = compute(i, Rm*j/10, tou)
+            if (j == 3):
+                temp1.append(temp[0])
+            temp2.append(temp[1])
+        output_mag_err.append(temp2)
+        output_mag_act.append(temp1)
+
+    with open("DC_offset/My_mag_err.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(['time(s)', '30%', '50%', '70%', '90%'])
+        writer.writerows(output_mag_err)
+
+    with open("DC_offset/My_mag_act.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(['time(s)', 'correct_value'])
+        writer.writerows(output_mag_act)
+
+    ### End of making dataset for varying mag ###
+
+    ### Fixed mag with varying tou ###
+    output_tou_err = []
+    output_tou_act = []
+    gamma = 150
+    for i in range(1, 15*N+1):
+        temp1 = [round(i*T, 4)]
+        temp2 = [round(i*T, 4)]
+        for j in range(1, 11, 3):
+            temp = compute(i, gamma, j/100)
+            if (j == 1):
+                temp1.append(temp[0])
+            temp2.append(temp[1])
+        output_tou_err.append(temp2)
+        output_tou_act.append(temp1)
+
+    with open("DC_offset/My_tou_err.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(['time(s)','10ms', '40ms', '70ms', '100ms'])
+        writer.writerows(output_tou_err)
+
+    with open("DC_offset/My_tou_act.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(['time(s)', 'correct_value'])
+        writer.writerows(output_tou_act)
+    ### End of making dataset for varying tou ###
---
+++ mydft.py
@@ -0,0 +1,39 @@
+import sys
+import math
+import time
+import csv
+
+N = 100  # number of samples per cycle
+
+
+def dft(wave):
+    x1 = (2/N)*(sum((wave[i-1]*math.cos(2*math.pi*i/N) for i in range(1, N+1))))
+    x2 = (2/N)*(sum((wave[i-1]*math.sin(2*math.pi*i/N) for i in range(1, N+1))))
+    mag = math.sqrt(math.pow(x1, 2)+math.pow(x2, 2))
+    phaseAngle = math.atan(x1/x2)
+    return mag, math.degrees(phaseAngle)
+
+
+# data gathering from csv file
+phase = []
+with open(sys.argv[1], 'r') as ifile:
+    ifile.readline()
+    for s in ifile.readlines():
+        phase.append(float(s.split(',')[1]))
+
+# cycle by cycle dft
+mCycle = int(len(phase)/N)
+output = []
+for cycle in range(mCycle):
+    params = dft(phase[N*cycle:N+N*cycle])
+    output.append([cycle, params[0], params[1]])
+    #print(cycle+1, params[0], params[1])
+
+with open('DC_offset/Mag_phasor.csv', 'w', newline='') as file: 
+    writer = csv.writer(file)
+    writer.writerows(output)
+'''
+# sliding window
+for i in range(len(phase)-N+1):
+    print(i+N-1, ' ', (i+N-1)/5000, ' ', dft(phase[i:i+N]))
+'''
