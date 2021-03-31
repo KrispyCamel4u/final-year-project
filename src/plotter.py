16088ba9d63ebbb72afcce19e9d5e53ce384cf7e
KrispyCamel4u <neerajjangra4u@gmail.com>
2021-03-31 11:11:26 +0530

    segregating scripts

---
+++ src/plotter.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import csv,sys
+
+filee = sys.argv[1]
+x = []
+ys = {}
+bypass = 1
+bpy = 1
+with open(filee) as ifile:
+    csv_reader = csv.reader(ifile, delimiter=',')
+    for row in csv_reader:
+        if bpy:
+            bpy = 0
+            continue
+        x.append(float(row[0]))
+        for i, ele in enumerate(row[1:]):
+            if bypass:
+                ys[i] = []
+            ys[i].append(float(ele))
+        bypass = 0
+plt.figure()
+plt.subplot(121)
+plt.plot(x, ys[0])
+plt.subplot(122)
+plt.plot(x, ys[len(ys)-1])
+plt.show()
